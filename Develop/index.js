31284419945a09bd4221b793cdf78896376cc4eb
Wampastomp99 <michaelheartbound99@hotmail.com>
2021-03-21 12:10:11 -0600

    more questons

--- Develop/index.js
+++ Develop/index.js
@@ -29,4 +29,29 @@ const questions = [
         name: "usage",
         message: "What is your projects usage?"
     },
-]
+    {
+        type: "input",
+        name: "licence",
+        message: "What is your licences/badge links?"
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "who contibuted to this project?"
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "How is this project tested?"
+    },
+    {
+        type: "input",
+        name: "username",
+        message: "What is your github user name?"
+    },
+    {
+        type: "input",
+        name: "repo",
+        message: "What is your repo link?"
+    },
+];
