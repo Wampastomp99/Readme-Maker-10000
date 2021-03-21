4ad6fc4ba6683d2fd39db2929530d1e07dad484a
Wampastomp99 <michaelheartbound99@hotmail.com>
2021-03-21 11:44:39 -0600

    questions

--- Develop/index.js
+++ Develop/index.js
@@ -4,5 +4,29 @@ const axios = require("axios");
 const generate = require('./utils/generateMarkdown');
 
 const questions = [
-    
+    {
+        type: "input",
+        name: "title",
+        message: "What is your projects title?"
+    },
+    {
+        type: "input",
+        name: "badge",
+        message: "What badge links do you want?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "What is your projects description?"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "What are your installation instructions"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "What is your projects usage?"
+    },
 ]
