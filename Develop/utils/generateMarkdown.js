ac435f4533013066f4007df5d7267148e4516558
Wampastomp99 <michaelheartbound99@hotmail.com>
2021-03-21 13:12:00 -0600

    markdown

---
+++ Develop/utils/generateMarkdown.js
@@ -0,0 +1,24 @@
+function generateMarkdown(data, githubInfo) {
+    return `
+  # **${data.title}**
+  
+  ${data.badge}
+  
+  ## Description 
+  
+  ${data.description}
+  
+  ## Table of contents
+  
+  - [Description](#Description)
+  - [Installation](#Installation)
+  - [Usage](#Usage)
+  - [Licence](#Licence)
+  - [Contributors](#Contributors)
+  - [Test](#Test)
+  - [Repository Link](#Repository)
+  - [GitHub Info](#GitHub)
+
+  ## Installation
+
+             ${data.installation}
